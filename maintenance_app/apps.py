a053438a28c988116625e772273639d84d526988
Vinchenzo98 <vinnyboardman@hotmail.co.uk>
2025-02-17 13:15:23 +0000

    boilerplate azure code

--- maintenance_app/apps.py
+++ maintenance_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HelloAzureConfig(AppConfig):
+class MaintenanceAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hello_azure'
+    name = 'maintenance_app'
--- quickstartproject/urls.py
+++ quickstartproject/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('hello_azure.urls')),
+    path('', include('maintenance_app.urls')),
     path('admin/', admin.site.urls),
 ]
